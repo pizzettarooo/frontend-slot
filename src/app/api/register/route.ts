44c7ea9695694c7e7565c8a9fbf6c9c68d92fa10
Vitale Benedetto Andrea <megadruido@gmail.com>
2025-04-03 09:49:27 +0200

    Setup iniziale del progetto

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,9 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string
 );
+
 
 export async function POST(req: Request) {
   const { username, password, wallet } = await req.json();
