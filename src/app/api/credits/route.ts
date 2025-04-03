d174b04f4ed73d0b17b056ad7b8d7586820778ac
Vitale Benedetto Andrea <megadruido@gmail.com>
2025-04-03 13:19:23 +0200

    Setup iniziale del progetto

--- src/app/api/credits/route.ts
+++ src/app/api/credits/route.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_SERVICE_ROLE_KEY as string
+  process.env.SUPABASE_ANON_KEY as string
 );
 
 
