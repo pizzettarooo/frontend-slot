4346d286f822eea88ed75cd3c4eca0e843b8b83e
Vitale Benedetto Andrea <megadruido@gmail.com>
2025-04-03 09:44:49 +0200

    Setup iniziale del progetto

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import "dotenv/config"; // assicura che le variabili .env vengano lette
 
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export async function POST(req: Request) {
